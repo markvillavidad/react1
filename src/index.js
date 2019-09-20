991248154922064446c080d5cc4cbc354053e939
Mark Vidad <mvidad@pelmorex.com>
2019-09-19 23:10:46 -0400

    Chapter 3: Creating FUNCTIONAL components

--- src/index.js
+++ src/index.js
@@ -8,18 +8,15 @@ let skiData = {
     goal: 100
 };
 
-class SkiDayCounter extends Component {
 
-    getPercent = decimal => {
-        return decimal * 100 + '%';
-    }
+const getPercent = decimal => {
+    return decimal * 100 + '%';
+}
+const calcGoalProgress = (total, goal) => {
+    return getPercent(total/goal);
+}
 
-    calcGoalProgress = (total, goal) => {
-        return this.getPercent(total/goal);
-    }
-
-    render() {
-        const {total, powder, backCountry, goal} = this.props;
+const SkiDayCounter = ({total, powder, backCountry, goal}) =>  {
         return (
             <section>
                 <div>
@@ -32,13 +29,44 @@ class SkiDayCounter extends Component {
                     <p>BackCountry Days: {backCountry}</p>
                 </div>
                 <div>
-                    <p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
+                    <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
                 </div>
 
             </section>
         )
-    }
 }
+
+// class SkiDayCounter extends Component {
+
+//     getPercent = decimal => {
+//         return decimal * 100 + '%';
+//     }
+
+//     calcGoalProgress = (total, goal) => {
+//         return this.getPercent(total/goal);
+//     }
+
+//     render() {
+//         const {total, powder, backCountry, goal} = this.props;
+//         return (
+//             <section>
+//                 <div>
+//                     <p>Total Days: {total}</p>
+//                 </div>
+//                 <div>
+//                     <p>Powder Days: {powder}</p>
+//                 </div>
+//                 <div>
+//                     <p>BackCountry Days: {backCountry}</p>
+//                 </div>
+//                 <div>
+//                     <p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
+//                 </div>
+
+//             </section>
+//         )
+//     }
+// }
 
 render(
     <SkiDayCounter
