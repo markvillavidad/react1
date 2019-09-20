8662a9def7b9826f6c193d3646ed4ae7fbc29dfa
Mark Vidad <mvidad@pelmorex.com>
2019-09-19 22:47:08 -0400

    Chapter 3: Adding component props

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ class Message extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello Everyone!</h1>
+                <h1 style={{color: this.props.color}}>
+                    {this.props.msg}
+                </h1>
+                <p>I'll check in {this.props.minutes} minutes</p>
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    <Message />,
+    <Message color="blue" msg="how are you?" minutes={5}/>,
     document.getElementById('root')
 )
