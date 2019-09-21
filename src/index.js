063e04b3ee06acf2aa9546ffeadc8776ecc2d575
Mark Vidad <mvidad@pelmorex.com>
2019-09-20 21:09:36 -0400

    Chapter 4: Composing components

--- src/index.js
+++ src/index.js
@@ -1,79 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {render} from 'react-dom';
 
-let skiData = {
-    total: 50,
-    powder: 20,
-    backCountry: 10,
-    goal: 100
-};
 
-
-const getPercent = decimal => {
-    return decimal * 100 + '%';
+const Book = ({title, author, pages}) => {
+    return (
+        <section>
+            <h2>{title}</h2>
+            <p>by: {author}</p>
+            <p>Pages: {pages} pages</p>
+        </section>
+    )
 }
-const calcGoalProgress = (total, goal) => {
-    return getPercent(total/goal);
-}
-
-const SkiDayCounter = ({total, powder, backCountry, goal}) =>  {
-        return (
-            <section>
-                <div>
-                    <p>Total Days: {total}</p>
-                </div>
-                <div>
-                    <p>Powder Days: {powder}</p>
-                </div>
-                <div>
-                    <p>BackCountry Days: {backCountry}</p>
-                </div>
-                <div>
-                    <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
-                </div>
-
-            </section>
-        )
-}
-
-// class SkiDayCounter extends Component {
-
-//     getPercent = decimal => {
-//         return decimal * 100 + '%';
-//     }
-
-//     calcGoalProgress = (total, goal) => {
-//         return this.getPercent(total/goal);
-//     }
-
-//     render() {
-//         const {total, powder, backCountry, goal} = this.props;
-//         return (
-//             <section>
-//                 <div>
-//                     <p>Total Days: {total}</p>
-//                 </div>
-//                 <div>
-//                     <p>Powder Days: {powder}</p>
-//                 </div>
-//                 <div>
-//                     <p>BackCountry Days: {backCountry}</p>
-//                 </div>
-//                 <div>
-//                     <p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
-//                 </div>
-
-//             </section>
-//         )
-//     }
-// }
 
 render(
-    <SkiDayCounter
-        total={skiData.total}
-        powder={skiData.powder}
-        backCountry={skiData.backCountry}
-        goal={skiData.goal}
-    />,
+    <div>
+        <Book title="The Sun Also Rises" author="Mark Vidad" pages={450} />
+        <Book title="El Filibusterismo" author="Jose Rizal" pages={300} />
+    </div>,
     document.getElementById('root')
 )
